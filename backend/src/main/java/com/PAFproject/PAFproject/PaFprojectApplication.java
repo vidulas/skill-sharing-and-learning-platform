02c66fec2b9ef3acf2b463e99585404310c66690
Vidula Gunasekara <vidulashavinda12@gmail.com>
2025-03-28 12:50:13 +0530

    Initial project setup: frontend and backend

---
+++ backend/src/main/java/com/PAFproject/PAFproject/PaFprojectApplication.java
@@ -0,0 +1,13 @@
+package com.PAFproject.PAFproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaFprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaFprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/PAFproject/PAFproject/PaFprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PAFproject.PAFproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaFprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
