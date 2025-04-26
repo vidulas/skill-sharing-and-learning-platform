957d5cedb430bd7475bca620aab054d3a987bee7
JaneDoe <semaldesilva2002@gmail.com>
2025-04-26 09:40:45 +0530

    initial adding

---
+++ skillshare/src/test/java/com/edu/skillshare/SkillshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.skillshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
