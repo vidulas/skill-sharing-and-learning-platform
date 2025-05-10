6f4426a8d58a0248dc2e1c0f6623be52219c99f0
AdithyaLiyanage <lrtadithyaliyanage@gmail.com>
2025-05-10 10:38:25 +0530

    modified topic model

--- backend/src/main/java/backend/model/Topic.java
+++ backend/src/main/java/backend/model/Topic.java
@@ -3,7 +3,6 @@ package backend.model;
 import jakarta.validation.constraints.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
