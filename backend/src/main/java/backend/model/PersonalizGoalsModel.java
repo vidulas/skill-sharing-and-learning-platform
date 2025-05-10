49c4ae3577e850bbc9b319a6575666a805c3d172
AdithyaLiyanage <lrtadithyaliyanage@gmail.com>
2025-05-10 10:35:56 +0530

    Modified Personalized Goals model

--- backend/src/main/java/backend/model/PersonalizGoalsModel.java
+++ backend/src/main/java/backend/model/PersonalizGoalsModel.java
@@ -2,7 +2,6 @@ package backend.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
-
 import java.util.Date;
 
 @Document(collection = "goals")
